de5fef46330206287eb6b07fe8c00d24bfcd6226
OXIDEN <oxiden.dd@gmail.com>
2013-07-05 00:57:07 +0900

    fix: homepage(default routing)

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Tweeter::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'shops#index'
 
   # See how all your routes lay out with "rake routes"
 
