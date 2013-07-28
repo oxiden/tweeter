3bfcc109b8745cc3b019467810160bc42d0ad1ff
OXIDEN <oxiden.dd@gmail.com>
2013-07-28 09:12:42 +0900

    fix: herokuさんから警告受けたのでassetsプリコンパイル時にRails初期化しないよう修正

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,8 @@ module Tweeter
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # prevent initialize & use database in precompile assets
+    config.assets.initialize_on_precompile = false
   end
 end
